f8db99ab4a80d35dcf842e99f0f5971e7cac544f
Josue Franco <josuefranco32@gmail.com>
2023-07-22 15:10:13 -0700

    added colors to the stroke

--- script.js
+++ script.js
@@ -8,14 +8,21 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55'
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 100;
+//ctx.globalCompositeOperation ='multiply'    //this adds different effects to the stroke
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue =0;
+let direction = true;
 
 function draw(e){
     if(!isDrawing)return; //stops the function from running when mouse not clicked
+
     console.log(e)
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
+    
     ctx.beginPath();
     //start from
     ctx.moveTo(lastX,lastY);
@@ -23,6 +30,19 @@ function draw(e){
     ctx.lineTo(e.offsetX,e.offsetY)
     ctx.stroke();
     [lastX,lastY]=[e.offsetX,e.offsetY]
+    hue++
+    if(hue>=360){
+        hue=0
+    }
+    if (ctx.lineWidth >= 100 || ctx.lineWidth <=1){
+        direction=!direction
+    }
+    if(direction){
+        ctx.lineWidth++;
+    }else{
+        ctx.lineWidth--;
+    }
+    
 }
 canvas.addEventListener('mousemove', draw)
 
